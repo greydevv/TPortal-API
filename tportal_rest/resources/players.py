5eb044ce4b99f906b5aab00646f91de6abfa7076
Greyson Murray <greyson.murray@gmail.com>
2021-11-26 21:20:51 -0500

    create resources

---
+++ tportal_rest/resources/players.py
@@ -0,0 +1,84 @@
+from flask import request, make_response
+from flask_restful import Resource
+from tportal_rest.ext import mongo
+from tportal_rest.exceptions import PlayerNotFoundError, DuplicateKeyError
+
+class Players(Resource):
+    def get(self):
+        players = mongo.db.players
+        pipeline = self.__build_pipeline(request.args)
+        data = list(players.aggregate(pipeline))
+        return data
+
+    def post(self):
+        if not request.json:
+            return make_response({'status':204}, 204)
+        players = mongo.db.players
+        dup_pids = self.__get_dup_pids(request.json)
+        if dup_pids:
+            raise DuplicateKeyError(data=dup_pids)
+        players.insert_many(request.json)
+        # return make_response({'status':200}, 200)
+        return {'status': 200}
+
+    def delete(self):
+        players = mongo.db.players
+        dup_pids = self.__get_dup_pids(request.json)
+        if dup_pids:
+            raise DuplicateKeyError(data=dup_pids)
+        pids = list(filter(lambda x: isinstance(x, str), request.json))
+        result = players.delete_many({'pid': {'$in':pids}})
+        if result.deleted_count > 0:
+            return {'status': 200}, 200
+            # return make_response({'status':200}, 200)
+        else:
+            return {'status': 204}, 204
+            print(make_response({'status':204}, 204))
+            return make_response({'status':204}, 204)
+
+    @staticmethod
+    def __build_pipeline(args):
+        pipeline = []
+        if args.get('q'):
+            query = args.get('q')
+            pipeline.append({'$search': {'text': {'query': query, 'path': {'wildcard': '*'}}}})
+        if args.get('position'):
+            pos = args['position']
+            pipeline.append({'$match': {'meta.position': {'$regex': f'^{pos}$', '$options': 'i'}}})
+        if args.get('division'):
+            pipeline.append({'$match': {'meta.division': int(args['division'])}})
+        if args.get('year'):
+            pipeline.append({'$match': {'meta.year': int(args['year'])}})
+        if args.get('limit') and args.get('limit').isnumeric():
+            pipeline.append({'$limit': int(args['limit'])})
+        pipeline.append({'$project': {'_id': False}})
+        return pipeline
+
+    @staticmethod
+    def __get_dup_pids(json):
+        players = mongo.db.players
+        dup_pids = []
+        if isinstance(json, list):
+            pids = json
+            if all(isinstance(e, dict) for e in json):
+                pids = [e['pid'] for e in json]
+            dup_pids = [dup['pid'] for dup in players.find({'pid': {'$in':pids}})]
+        else:
+            if players.find_one({'pid':json['pid']}):
+                dup_pids = [json['pid']]
+        return dup_pids
+
+class Player(Resource):
+    def get(self, pid):
+        players = mongo.db.players
+        data = players.find_one({'pid': pid}, {'_id': False})
+        if data:
+            return data
+        raise PlayerNotFoundError(data=pid)
+
+    def delete(self, pid):
+        players = mongo.db.players
+        result = players.delete_one({'pid': pid})
+        if result.deleted_count == 1:
+            return make_response({'status': 200}, 200)
+        raise PlayerNotFoundError(data=pid)
