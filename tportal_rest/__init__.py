5dd72634181301b19d34e4c2e81e333341a5a46f
Greyson Murray <greyson.murray@gmail.com>
2021-11-26 21:21:53 -0500

    hook up resources

--- tportal_rest/__init__.py
+++ tportal_rest/__init__.py
@@ -1,6 +1,16 @@
-from flask import Flask
-from dotenv import load_dotenv
 import os
+from flask import Flask
+from flask_restful import Api
+from dotenv import load_dotenv
+from tportal_rest.ext import mongo
+from tportal_rest.resources.players import Players, Player
+from tportal_rest.exceptions import ApiException
+
+class TPortalREST(Api):
+    def handle_error(self, e):
+        if isinstance(e, ApiException):
+            return e.get_response()
+        return super().handle_error(e)
 
 def get_mongo_uri():
     load_dotenv()
@@ -13,4 +23,9 @@ def create_app():
     app = Flask(__name__)
     app.config['MONGO_URI'] = get_mongo_uri()
 
+    mongo.init_app(app)
+
+    api = TPortalREST(app)
+    api.add_resource(Players, '/v1/players')
+    api.add_resource(Player, '/v1/players/<string:pid>')
     return app
