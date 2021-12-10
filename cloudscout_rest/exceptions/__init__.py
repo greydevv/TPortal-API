8a052abd40c61b98615c0492c4c4de91a03ff823
Greyson Murray <greyson.murray@gmail.com>
2021-12-10 12:09:44 -0500

    impl user & login exceptions

--- cloudscout_rest/exceptions/__init__.py
+++ cloudscout_rest/exceptions/__init__.py
@@ -5,12 +5,17 @@ class ApiException(Exception):
     def __init__(self, **kwargs):
         self.response = {}
         if hasattr(self, 'message'):
-            self.response['message'] = self.message
+            self.response['msg'] = self.message
+        elif hasattr(self, 'msg'):
+            self.response['msg'] = self.msg
         if kwargs:
             self.response = self.response | kwargs
 
     def get_response(self) -> Response:
         return make_response(self.response, self.code)
+
+
+# PLAYERS
 
 class DuplicateKeyError(ApiException):
     """
@@ -18,7 +23,7 @@ class DuplicateKeyError(ApiException):
     create a duplicate key entry if it were to be entered into the database.
     """
     code = 409
-    message = 'Could not add players to database as doing so would cause duplicate entries.'
+    msg = 'Could not add players to database as doing so would cause duplicate entries'
 
 class PlayerNotFoundError(ApiException):
     """
@@ -26,4 +31,32 @@ class PlayerNotFoundError(ApiException):
     exist in the database.
     """
     code = 404
-    message = 'Could not find the player with the specified pid.'
+    msg = 'Could not find the player with the specified pid'
+
+
+# USERS
+
+class UserNotFoundError(ApiException):
+    """
+    Raised when a query for a user (by uid) is made, but the uid does not 
+    exist in the database.
+    """
+    code = 404
+    msg = 'Could not find the user with the specified pid'
+
+
+# LOGIN
+
+class UserNotRegisteredError(ApiException):
+    """
+    Raised when a user attempts to login with an email that does not exist in the database.
+    """
+    code = 404
+    msg = 'Email not registered'
+
+class InvalidLoginError(ApiException):
+    """
+    Raised when a user attmepts to login with invalid credentials.
+    """
+    code = 401
+    msg = 'Incorrect username or password'
