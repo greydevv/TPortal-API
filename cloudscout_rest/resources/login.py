25dc1749e9c673222e8e654e3c574e4b107a8705
Greyson Murray <greyson.murray@gmail.com>
2021-12-17 10:50:29 -0500

    impl refresh and introspect endpoints

--- cloudscout_rest/resources/login.py
+++ cloudscout_rest/resources/login.py
@@ -1,14 +1,15 @@
-import datetime as dt
 from flask import request
 from flask_restful import Resource
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required
 from cloudscout_rest.ext import bcrypt, jwt, mongo
-from cloudscout_rest.exceptions import UserNotRegisteredError, InvalidLoginError
-from cloudscout_rest.schema import LOGIN
-from cloudscout_rest.common.validate_json import assertjson
+from cloudscout_rest.exceptions import UserNotRegisteredError, InvalidLoginError, NoAuthSuppliedError
 
 class Login(Resource):
     def post(self):
+        print(request.headers)
+        # print("LOGGING USER IN: ", request.authorization)
+        if not request.authorization:
+            raise NoAuthSuppliedError
         users = mongo.db.users
         auth = request.authorization
         login_obj = users.find_one(
@@ -22,5 +23,18 @@ class Login(Resource):
         if not bcrypt.check_password_hash(login['password'], auth['password']):
             raise InvalidLoginError
         
-        jwt_tok = create_access_token(identity=login['email'], expires_delta=dt.timedelta(minutes=60))
-        return {'token': jwt_tok}, 200
+        jwt_tok = create_access_token(identity=login['email'])
+        ref_jwt_tok = create_refresh_token(identity=login['password'])
+        return {'access_token': jwt_tok, 'refresh_token': ref_jwt_tok}, 200
+
+class Refresh(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        identity = get_jwt_identity()
+        jwt_tok = create_access_token(identity=identity)
+        return {'access_token': jwt_tok}
+
+class Introspect(Resource):
+    @jwt_required()
+    def post(self):
+        return '', 201
